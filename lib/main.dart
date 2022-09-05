c02e47ac6e7bbc2c31105f9b84118e92255245ed
kodepirate <aniket.das9062@gmail.com>
2022-09-05 21:21:15 +0530

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); //Method to run the app
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key); //Constructor
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    int days = 30;
+
+    return  MaterialApp(
       home: Material(
         child: Center(
-          child: Text('Hello World'),
+          child: Text('Hello World $days'),  
         ),
       ), 
     );
