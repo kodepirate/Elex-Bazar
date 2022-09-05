b1f0e7529042a583feee59c4cdefd50bb4e85549
kodepirate <aniket.das9062@gmail.com>
2022-09-05 21:01:51 +0530

    okk

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Text('Hello World'),
+        ),
+      ), 
+    );
+}
 }
